e9250536ed7804eeda0fad517109e6112e3a7e8d
cheryl-pope <cheryl.pope@adelaide.edu.au>
2020-12-24 17:24:39 +1030

    Update parsons-puzzle.js
    linking up buttons with correct tags from semantics.json
    
    restructuring format - removal of code that hid puzzle on submit.  Puzzle and results should be visible on same page as there is only one puzzle.

--- parsons-puzzle.js
+++ parsons-puzzle.js
@@ -1,8 +1,6 @@
 var H5P = H5P || {};
 
-H5P.ParsonsPuzzle = (function ($, ParsonsJS) {
-
-    /**
+/**
      * @class H5P.ParsonsPuzzle
      * @param       {Object} options  Object with current data and configurations
      * @param       {integer} id      Unique identifier
@@ -10,44 +8,41 @@ H5P.ParsonsPuzzle = (function ($, ParsonsJS) {
      *
      * @returns {Object} ParsonsPuzzle instance of a ParsonsPuzzle
      */
-     function ParsonsPuzzle(options, id, data) {
-        this.$ = $(this);
-        this.id = id;
-        this.data = data;
+     H5P.ParsonsPuzzle = function (options, id) {
 
-        var defaults = {
-          passPercentage: 50,
 
-            overallFeedback: [],
-            finishButtonText: 'Finish',
-            solutionButtonText: 'Show solution'
-          },
-          override: {},
-          disableBackwardsNavigation: false
-        };
+       var $ = H5P.jQuery;
+       var self = this;
 
-        this.options = $.extend(true, {}, defaults, options); // defined in semantics.json
-        // this.quiz = this.options.quiz;
-        this.$startQ = $('<button/>', { 'class': "startQuiz", 'text': "	Start Quiz ?" });
-        this.$inner = $('<div/>', {
-          class: "h5p-inner"
-        });
-        // this.$endQ = $('<button/>', { 'class': "endQuiz", 'text': "submit Quiz " });
+       if (!(this instanceof H5P.ParsonsPuzzle)) {
+        return new H5P.ParsonsPuzzle(options, id);
+      }
+
+      self.id = id;
+
+      var defaults = {
+        overallFeedback: [],
+      },
+      disableBackwardsNavigation: false
+    };
+
+        self.options = $.extend(true, {}, defaults, options); // defined in semantics.json
+
 
         //score create
-        this.score = 0;
-        this.Maxscore = 0;
-        this.finals;
-        this.totals;
-        this.scoreString = "";
-        this.success;
-        this.scoreBar;
-        this.addTemplate();
+        self.score = 0;
+        self.finals;
+        self.totals;
+        self.scoreString = "";
+        self.success;
+        self.scoreBar;
+  
+        self.createResultTemplate();
       }
 
 
       /**add templates  */
-      ParsonsQuiz.prototype.addTemplate = function () {
+      ParsonsQuiz.prototype.createResultTemplate = function () {
         var resulttemplate =
         '<div class="questionset-results">' +
         '  <div class="greeting"><%= message %></div>' +
@@ -62,7 +57,7 @@ H5P.ParsonsPuzzle = (function ($, ParsonsJS) {
         '  <div class="result-text"><%= resulttext %></div>' +
         '  <% } %>' +
         '</div>';
-        this.endTemplate = new EJS({ text: resulttemplate });
+        self.endTemplate = new EJS({ text: resulttemplate });
       }
 
 
@@ -75,60 +70,61 @@ H5P.ParsonsPuzzle = (function ($, ParsonsJS) {
      ParsonsQuiz.prototype.attach = function ($container) {
 
         // set container
-        this.$container = $container;
+        self.$container = $container;
         $container.addClass('h5p-parsons');
-        this.$inner.appendTo($container);
 
         // add quiz title and description
-        $('<div/>', { "text": this.data.metadata.title, "id": "title" }).appendTo(this.$inner);
+        $('<div/>', { class: "h5p-inner", "text": self.data.metadata.title, "id": "title" }).appendTo($container);
 
            // add meta data of the question
-            var template =
-            '<p class="problemDescription"><%= problemDescription %></p>' +
-            '<p class="codeLanguage" id="language-<%= index %>">' +
-            '    <i class="fas fa-globe-asia"> language: </i>' +
-            '    <%= codeLanguage %> ' +
-            '</p>' +
-            '<div class="sortable-code" id="sortableTrash"> </div>' +
-            '<div class="sortable-code" id="sortable"> </div>' +
-            '<div style="clear:both;"></div>' +
-            '' +
-            '<div class="actions">' +
-            '    <button type="button" role="button" class="h5p-joubelui-button newInstance" id="newInstanceLink-<%= index %>"><%= button1Title %></button>' +
-            '    <button type="button" role="button" class="h5p-joubelui-button feedback" id="feedbackLink-<%= index %>"><%= button2Title %></button>' +
-            '</div>' +
-            '<div class="actions" style="margin-top: 70px;">' +
-            '    <button type="button" role="button" class="h5p-joubelui-button submit endQuiz" id="submitLink-<%= index %>"><%= buttonSubmit %></buttona>' +
-            '</div>';
+           var template =
+           '<p class="problemDescription"><%= problemDescription %></p>' +
+           '<p class="codeLanguage" id="language-<%= index %>">' +
+           '    <i class="fas fa-globe-asia"> language: </i>' +
+           '    <%= codeLanguage %> ' +
+           '</p>' +
+           '<div class="sortable-code" id="sortableTrash"> </div>' +
+           '<div class="sortable-code" id="sortable"> </div>' +
+           '<div style="clear:both;"></div>' +
+           '' +
+           '<div class="actions">' +
+           '    <button type="button" role="button" class="h5p-joubelui-button submit endQuiz" id="submitLink-<%= index %>"><%= buttonSubmit %></button>' +
+           ' <% if (problem.code.enableRetryButton) { %> ' +
+                  '<button type="button" role="button" class="h5p-joubelui-button newInstance" id="newInstanceLink-<%= index %>"><%= button1Title %></button>' +
+            '  <% } %> ' +
+             ' <% if (problem.code.enableSolutionButton) { %> ' +
+           '    <button type="button" role="button" class="h5p-joubelui-button feedback" id="feedbackLink-<%= index %>"><%= button2Title %></button>' +
+            '  <% } %> ' +
+           '</div>' 
 
-            var ejs_template = new EJS({ text: template });
-            let html = ejs_template.render({
-              problemDescription: puzzleInstructions,
-              codeLanguage: problem.code.code_language,
-              button1Title: problem.code.tryAgain,
-              button2Title: problem.code.solutionButtonText,
-              buttonSubmit: problem.code.checkAnswer,
-            });
-            console.log(html);
-            $(html).appendTo(parsonsjs.$question);
+           var ejs_template = new EJS({ text: template });
+           let html = ejs_template.render({
+            problemDescription: puzzleInstructions,
+            codeLanguage: problem.code.code_language,
+            button1Title: problem.code.tryAgain,
+            button2Title: problem.code.solutionButtonText,
+            buttonSubmit: problem.code.checkAnswer,
+          });
+           console.log(html);
+           $(html).appendTo(parsonsjs.$question);
 
 
             //
             // question content
 
             var code_line = problem.code.code_block;
-              var final_param = {};
-              var default_setting = {
-                'sortableId': 'sortable',
-                'trashId': 'sortableTrash',
-              };
-                var normal = { 'feedback_cb': displayErrors };
+            var final_param = {};
+            var default_setting = {
+              'sortableId': 'sortable',
+              'trashId': 'sortableTrash',
+            };
+            var normal = { 'feedback_cb': displayErrors };
 
-              var parson = new ParsonsJS.ParsonsWidget(final_param, i)
-              self.parsonList.push(parson);
+            var parson = new ParsonsJS.ParsonsWidget(final_param, i)
+            self.parsonList.push(parson);
 
-              parson.init(code_line);
-              parson.shuffleLines();
+            parson.init(code_line);
+            parson.shuffleLines();
 
             // newInstance and feedback buttons
             $("<div/>", { "style": "clear:both;" }).appendTo(parsonsjs.$question);
@@ -136,13 +132,13 @@ H5P.ParsonsPuzzle = (function ($, ParsonsJS) {
             $('<div/>', { "id": "unittest-" + i }).appendTo(parsonsjs.$question);
           }
           $(".newInstance").on('click', function (event) {
-            var currentId = $(this).attr('id');
+            var currentId = $(self).attr('id');
             var currentIndex = currentId.substr(currentId.length - 1);
             event.preventDefault();
             self.parsonList[currentIndex].shuffleLines();
           });
           $(".feedback").on('click', function (event) {
-            var currentId = $(this).attr('id');
+            var currentId = $(self).attr('id');
             var currentIndex = currentId.substr(currentId.length - 1);
             console.log("feedback : " + currentIndex + "is ongoing");
             event.preventDefault();
@@ -152,17 +148,13 @@ H5P.ParsonsPuzzle = (function ($, ParsonsJS) {
             if (self.parsonList[currentIndex].correct == true) {
               self.score += 1;
             }
-            if (fb.success) { alert("Good, you solved this question!"); }
           });
         //submit button to submit the quiz form
         // self.$endQ.appendTo(self.$inner);
         $(".endQuiz").click(function () {
-            /**attach result page */
+          /**attach result page */
             // Trigger finished event.
             self.finals = self.score;
-            self.totals = self.Maxscore;
-            self.success = ((100 * self.finals / self.totals) >= self.options.passPercentage);
-
             self.scoreString = H5P.Question.determineOverallFeedback(self.options.endGame.overallFeedback, self.finals / self.totals);
 
             self.displayResults();
@@ -171,29 +163,26 @@ H5P.ParsonsPuzzle = (function ($, ParsonsJS) {
           });
         /**start display result setting */
         self.displayResults = function () {
-          this.triggerXAPICompleted(this.finals, this.totals, this.success);
+          self.triggerXAPICompleted(self.finals, self.totals, self.success);
 
           var eparams = {
-            message: this.options.endGame.showResultPage ? this.options.endGame.message : this.options.endGame.noResultMessage,
-            comment: this.options.endGame.showResultPage ? (this.success ? this.options.endGame.oldFeedback.successGreeting : this.options.endGame.oldFeedback.failGreeting) : undefined,
-            resulttext: this.options.endGame.showResultPage ? (this.success ? this.options.endGame.oldFeedback.successComment : this.options.endGame.oldFeedback.failComment) : undefined,
-            finishButtonText: this.options.endGame.finishButtonText,
+            message: self.options.endGame.showResultPage ? self.options.endGame.message : self.options.endGame.noResultMessage,
+            comment: self.options.endGame.showResultPage ? (self.success ? self.options.endGame.oldFeedback.successGreeting : self.options.endGame.oldFeedback.failGreeting) : undefined,
+            resulttext: self.options.endGame.showResultPage ? (self.success ? self.options.endGame.oldFeedback.successComment : self.options.endGame.oldFeedback.failComment) : undefined,
+            finishButtonText: self.options.endGame.finishButtonText,
           };
 
             // Show result page.
-            this.$container.children().hide();
-            this.$container.append(this.endTemplate.render(eparams));
-
-            if (this.options.endGame.showResultPage) {
-              scoreBar = this.scoreBar;
+            self.$container.append(self.endTemplate.render(eparams));
+              scoreBar = self.scoreBar;
               if (scoreBar === undefined) {
-                scoreBar = H5P.JoubelUI.createScoreBar(this.totals);
+                scoreBar = H5P.JoubelUI.createScoreBar(self.totals);
               }
-              scoreBar.appendTo($('.feedback-scorebar', this.$container));
-              $('.feedback-text', this.$container).html(this.scoreString);
+              scoreBar.appendTo($('.feedback-scorebar', self.$container));
+              $('.feedback-text', self.$container).html(self.scoreString);
 
 
-              this.trigger('resize');
+              self.trigger('resize');
             };
             /**end display result setting */
 
